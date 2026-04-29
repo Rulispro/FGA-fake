33e3873638d05c8a8481adbfcbfcce33a84b3923
Rulispro <95007142+Rulispro@users.noreply.github.com>
2026-04-29 15:48:21 +0700

    Update scrapegroups.js

--- dashboard/scrapegroups.js
+++ dashboard/scrapegroups.js
@@ -125,15 +125,15 @@ const name = text.split('\n')[0];
   const page = await browser.newPage();
 
 //setuseragent 
-  await page.setUserAgent(
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-);
+ // await page.setUserAgent(
+ // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+//);
 
-  await page.setViewport({
-  width: 1366,
-  height: 768,
-  isMobile: false
-});
+ // await page.setViewport({
+ // width: 1366,
+//  height: 768,
+ // isMobile: false
+//});
 
  // BARU recorder dibuat
 //const recorder = new PuppeteerScreenRecorder(page);
