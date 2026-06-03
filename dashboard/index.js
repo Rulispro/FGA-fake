6502ba3cbf604ab667b080c2c66a2c903dbb1d7a
Rulispro <95007142+Rulispro@users.noreply.github.com>
2026-06-03 23:18:40 +0700

    Update index.js

--- dashboard/index.js
+++ dashboard/index.js
@@ -3478,7 +3478,7 @@ console.log("✅ schedule.json berhasil dibuat (group by date)");
   // CLOSE BROWSER PER BATCH
   // ===============================
   const delayBatch =
-  60000 + Math.floor(Math.random() * 90000);
+  60000 + Math.floor(Math.random() * 60000);
 
 console.log(
   `🛑 Delay batch ${Math.floor(delayBatch / 1000)} detik`
